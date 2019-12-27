488a7882628f44d2c43e3f9d47f4c00eae1589c8
studiojenga <35477477+studiojenga@users.noreply.github.com>
2019-12-27 12:36:11 +0900

    31st commit

--- js/script.js
+++ js/script.js
@@ -1441,7 +1441,7 @@ window.onload = function(){
 			text01 = '';
 			div.style.visibility = 'hidden';
 		}
-		textNode.nodeValue = '大島\n大橋';
+		textNode.nodeValue = '花井\n拓';
 		//textNode.nodeValue = text01;
 		//selectedObject = sel;
 		//return sel;
