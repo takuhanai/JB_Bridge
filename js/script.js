634602ddd699087217e4263fe154a32d515c93ee
taku <takuhanai@yahoo.co.jp>
2020-02-10 23:01:58 +0900

    81st commit

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ window.onload = function(){
   // variables
 	let resourcePath = './resource/';
 	let title = 'oshima_bridge';
-	//let scene_name = 'oshima_bridge';
-	let scene_name = 'tower_01';
+	let scene_name = 'oshima_bridge';
+	//let scene_name = 'tower_01';
 	//let FPS;
 	let drawMode = 0;//0: draw all, 1: omit window, 2: omit window and roof
 	let eText = document.getElementById('text');
